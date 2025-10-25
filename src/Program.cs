94014775b7c545ad9bd29aed987e944687e0b6ab
Guilherme Agunzo <g.agunzo@gmail.com>
2025-10-25 10:54:12 -0300

    add initial migration

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,13 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(o =>
+{
+    o.UseSqlite("Data Source=app.db");
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
